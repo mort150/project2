4bbc0706f7f16abfff8541109805c24199cb8923
mort150 <thanhdtgch190775@fpt.edu.vn>
2021-08-21 10:05:44 +0700

    make it better

--- app.js
+++ app.js
@@ -95,6 +95,6 @@ function requireLogin(req, res, next) {
     }
 
 }
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 console.log('Server is running: ', PORT);
